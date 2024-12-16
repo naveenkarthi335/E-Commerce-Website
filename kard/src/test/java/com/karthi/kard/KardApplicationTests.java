0b834e895b29d82a7640c2cc3d50750be1564f05
Naveen karthi <160599603+naveenkarthi335@users.noreply.github.com>
2024-12-16 18:53:36 +0530

    Add files via upload

---
+++ kard/src/test/java/com/karthi/kard/KardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthi.kard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
